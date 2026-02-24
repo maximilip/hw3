181e95566de28845de1f35412196007397a82910
maximilip <max.prasad@kellogg.northwestern.edu>
2026-02-24 02:45:12 +0000

    places-mvc3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get("/places", { :controller => "places", :action => "index" })
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  
+  resources "places"
+    resources "entries"
+  end
 end
--- scripts/create_data.rb
+++ scripts/create_data.rb
@@ -5,3 +5,15 @@ Place.destroy_all
 mexico = Place.new
 mexico["name"] = "Mexico City"
 mexico.save
+
+charleston = Place.new
+charleston["name"] = "Charleston"
+charleston.save
+
+beijing = Place.new
+beijing["name"] = "Beijing"
+beijing.save
+
+amsterdam = Place.new
+amsterdam["name"] = "Amsterdam"
+amsterdam.save
